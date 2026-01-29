c7ffed4f454bf21d9c262cea0e149aad4c33e4d9
Sergio García <sergiogarcia.php@gmail.com>
2026-01-28 22:04:46 -0600

    Add model and migrate for Generator

---
+++ app/Models/Generator.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Generator extends Model
+{
+    protected $fillable = [
+        'generator_brand',
+        'generator_model',
+        'serial_number',
+    ];
+}
