faf594cbb36579422fd09adf5afffe3b1cb46b77
Sergio García <sergiogarcia.php@gmail.com.>
2026-05-01 15:58:44 -0600

    Fillable --Model=Team

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -2,18 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+#[Fillable(['name', 'description', 'is_active'])]
 class Team extends Model
 {
     /** @use HasFactory<\Database\Factories\TeamFactory> */
     use HasFactory;
-    protected $fillable = [
-        'name',
-        'description',
-        'is_active',
-    ];
     protected $casts = [
         'is_active' => 'boolean',
     ];
