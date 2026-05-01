0e2830d4b516801a445465d6700e74481c15d225
Sergio García <sergiogarcia.php@gmail.com.>
2026-05-01 13:57:12 -0600

    Auth/Laravel UI

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
+});
+
+Auth::routes();
+
+Route::middleware(['auth'])->group(function (){
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
 });
